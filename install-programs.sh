f18ca71c36afc5dd251ba5bb8c64490a72f09ada
joel body <joel.body@proton.me>
2025-03-04 13:45:10 +0000

    few changes

--- install-programs.sh
+++ install-programs.sh
@@ -1,7 +1,8 @@
-git clone https://github.com/Oelnbod/useful-config-files
 apt update
-apt install emacs curl snap snapd python3-* nc pipx elinks
+apt install emacs curl snap snapd python3-* netcat pipx elinks -y
+git clone https://github.com/Oelnbod/useful-config-files
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 snap install btop
-cp useful-config-file/init.el /home/$USER/.emacs.d/init.el
+cp ./useful-config-file/init.el /home/$USER/.emacs
+
 
